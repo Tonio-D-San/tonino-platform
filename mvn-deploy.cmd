4628fde42c5f47e5dd5cd2da2ed9e62251b69a15
Antonio Sansonne <asansonne@cybsec.it>
2026-09-21 16:14:54 +0200

    update deploy script

--- mvn-deploy.cmd
+++ mvn-deploy.cmd
@@ -1,1 +1,13 @@
-mvn clean deploy -DskipTests -pl ., libs/common-core, libs/common-graphql, libs/common-jpa, libs/common-keycloak, libs/common-people-core, libs/common-people-graphql-jpa, libs/common-people-rest-jpa, libs/common-rest
+@echo off
+setlocal
+
+set "JAVA_HOME=%USERPROFILE%\.jdks\corretto-25.0.4.1"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+call "%~dp0mvnw.cmd" ^
+  -s "%USERPROFILE%\.m2\settings-tonino.xml" ^
+  -Dmaven.repo.local="%USERPROFILE%\.m2\repository" ^
+  clean deploy ^
+  -DskipTests ^
+  -pl .,libs/common-core,libs/common-graphql,libs/common-jpa,libs/common-keycloak,libs/common-people-core,libs/common-people-graphql-jpa,libs/common-people-rest-jpa,libs/common-rest ^
+  -f "%~dp0pom.xml"
